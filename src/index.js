fb80138cb76816c320140e56a502b8fa23194bf5
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-06 12:23:09 +0545

    constructor method called by React, everytime you make a component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Example:
+// The constructor methods is called, by React, every time you make a component:
+
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {favoritecolor: "red"};
+    }
+    render(){
+        return (
+            <h1>My Favorite Color is  {this.state.favoritecolor}</h1>
+        );
+    }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
