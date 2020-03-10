db9d0aa59d9632095ae6d8923084b256eebf3de0
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-10 08:19:33 +0545

    componentDidUpdate

--- src/index.js
+++ src/index.js
@@ -2,34 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Example:
-// Use the getSnapshotBeforeUpdate() method to find out what the state object looked like before
-// the update:
+// The compountDidUpdate method is called after the update has been rendered in the DOM:
 
-class Header extends React.Component {
+class Header extends React.Component {  
     constructor(props){
         super(props);
         this.state = {favoritecolor: "red"};
     }
 
-    componentDidMount = () => {
+    componentDidMount() {
         setTimeout(() => {
         this.setState({favoritecolor: "yellow"}); 
         }, 2000);
     }
-    getSnapshotBeforeUpdate(prevProps, prevState){
-        document.getElementById("div1").innerHTML = "Before the updated, the favorite was " + prevState.favoritecolor;
-    }
 
     componentDidUpdate(){
-        document.getElementById("div2").innerHTML = "The updated favorite is " + this.state.favoritecolor;
+        document.getElementById("mydiv").innerHTML = "The updated favorite is "
+         + this.state.favoritecolor;
     }
     
     render(){
         return (
             <div>
             <h1>My Favorite Color is  {this.state.favoritecolor}</h1>
-            <div id="div1"></div>
-            <div id="div2"></div>
+            <div id="mydiv"></div>
             </div>
         );
     };
